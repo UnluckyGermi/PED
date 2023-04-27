a489e1e63dabe58819de1700434edeb91ac0aa35
UnluckyGermi <gre4@alu.ua.es>
2023-04-27 20:22:19 +0200

    Entrega1

--- evalua.sh
+++ evalua.sh
@@ -6,14 +6,13 @@
 #
 
 #Si tus ficheros de prueba no comienzan por "tad", cambia "tad*.cpp" por el prefijo de tus ficheros, por ejemplo "prueba*.cpp":
-for FILE in $1/*/tad*.cpp ; do 
+for FILE in $1/tad*.cpp ; do 
 	echo -e "\e[32mEvaluando $FILE \e[0m"
 	
 	cp $FILE src/tad.cpp 
         rm -f ./tad
 	make
 	./tad > $FILE.out
-  valgrind --tool=memcheck --leak-check=full ./tad &> /dev/null
   if [ "$?" == "1" ]; then
     echo -e "\e[31mERROR DE MEMORIA\e[0m"
   fi
